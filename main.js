72435cf15971fc5e09e0f3cf60bcb0347b44753d
George <sgeorge7731@gmail.com>
2019-04-11 12:01:16 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+let arr = ['plum.png', 'orange.jpeg', 'bitch'];
+
+//document.write(arr[0], arr[2]);
+
+//alert(arr);
+
+//let answer = prompt('you want to enter?', '');
+
+//document.write(typeof(arr));
+
+// document.write(2 != 5);
+// document.write(' ');
+// document.write('2' === 5);
+// document.write(' ');
+// document.write('2' == '2');
+
+document.getElementById('first').value; 
